56b35c96c226b1716bdddcf46968ac3eb20af9d2
JustinBrownRenovo <jbrown@renovosolutions.com.au>
2024-05-14 16:06:44 +1000

    Implement hover effect

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,10 +20,18 @@ export default function RootLayout({
         <nav>
           <h3 className="top-bar">
             <div style={{ display: 'flex', justifyContent: 'right', alignItems: 'right'}}>
-              <Link href="/" style={{marginRight: '20px'}}>ABOUT</Link>
-              <Link href="/bio" style={{marginRight: '20px'}}>CV</Link>
-              <Link href="/users" style={{marginRight: '20px'}}>USERS</Link>
-              <Link href="/portfolio" style={{marginRight: '20px'}}>PORTFOLIO</Link>
+              <button className="click">
+                <Link href="/" style={{marginRight: '20px'}}>ABOUT</Link>
+              </button>
+              <button className="click">
+                <Link href="/bio" style={{marginRight: '20px'}}>CV</Link>
+              </button>
+              <button className="click">
+                <Link href="/users" style={{marginRight: '20px'}}>USERS</Link>
+              </button>
+              <button className="click">
+                <Link href="/portfolio" style={{marginRight: '20px'}}>PORTFOLIO</Link>
+              </button>
             </div>
           </h3>
         </nav>
